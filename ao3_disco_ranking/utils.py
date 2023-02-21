e015c58efb557b34c0ba00fde509a928ccf31b0f
Kevin Alex Zhang <me@kevz.dev>
2023-02-21 00:08:33 -0800

    Fix rnk

--- ao3_disco_ranking/utils.py
+++ ao3_disco_ranking/utils.py
@@ -9,12 +9,14 @@ from ao3_disco_ranking.models import BaseModel, SecondModel
 
 def score(model: BaseModel, path_to_jsonl: str):
     """Compute the discounted cumulative gain."""
+    with open("data/works_collections.pkl", "rb") as fin:
+        works, _ = pickle.load(fin)
     ndcg = []
     with open(path_to_jsonl, "rt") as fin:
         for x in tqdm(map(json.loads, fin), "eval"):
             work = x["work"]
             candidates = x["candidates"]
-            prediction = model.rank(work, candidates)
+            prediction = model.rank(work, candidates, works)
             y_true, y_pred = [], []
             for work, score in prediction:
                 y_true.append(candidates[work])
